01dc13a3d030b0d78c407d4f25228f194b245bf8
JuanDMeGon <juandmegon@gmail.com>
2017-02-16 00:52:45 -0500

    Implementando las Propiedades de User

--- app/User.php
+++ app/User.php
@@ -9,13 +9,26 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const USUARIO_VERIFICADO = '1';
+    const USUARIO_NO_VERIFICADO = '0';
+
+    const USUARIO_ADMINISTRADOR = 'true';
+    const USUARIO_REGULAR = 'false';
+
+
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
     /**
@@ -24,6 +37,23 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token',
     ];
+
+    public function esVerificado()
+    {
+        return $this->verified == User::USUARIO_VERIFICADO;
+    }
+
+    public function esAdministrador()
+    {
+        return $this->admin == User::USUARIO_ADMINISTRADOR;
+    }
+
+    public static function generarVerificationToken()
+    {
+        return str_random(40);
+    }
 }
