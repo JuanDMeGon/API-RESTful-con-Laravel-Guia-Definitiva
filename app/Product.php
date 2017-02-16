eb20107f9bde9cb5f0ddeaa77987a8a1657fb26b
JuanDMeGon <juandmegon@gmail.com>
2017-02-16 00:22:08 -0500

    Implementando las Propiedades para Product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+	const PRODUCTO_DISPONIBLE = 'disponible';
+	const PRODUCT_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+    	'name',
+    	'description',
+    	'quantity',
+    	'status',
+    	'image',
+    	'seller_id',
+    ];
+
+    public function estaDisponible()
+    {
+    	return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
 }
