3c380d04fb61a72c795cd832296b82b461b0a0e5
JuanDMeGon <juandmegon@gmail.com>
2017-02-17 01:53:57 -0500

    Usando los Factories Desde el DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        $cantidadUsuarios = 200;
+        $cantidadCategorias = 30;
+        $cantidadProductos = 1000;
+        $cantidadTransacciones = 1000;
+
+
+        factory(User::class, $cantidadUsuarios)->create();
+        factory(Category::class, $cantidadCategorias)->create();
+
+		factory(Product::class, $cantidadTransacciones)->create()->each(
+			function ($producto) {
+				$categorias = Category::all()->random(mt_rand(1, 5))->pluck('id');
+
+				$producto->categories()->attach($categorias);
+			}
+		);        
+
+        factory(Transaction::class, $cantidadTransacciones)->create();
     }
 }
