7d09f0e2f6a2ad2d697f0240d2adf21097530cf7
JuanDMeGon <juandmegon@gmail.com>
2017-02-17 00:36:22 -0500

    Creando el Factory de Product

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Product;
 use App\Category;
 
 /*
@@ -34,5 +35,18 @@ $factory->define(Category::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        // 'seller_id' => User::inRandomOrder()->first()->id,
+        'seller_id' => User::all()->random()->id,
     ];
 });
