28b56a3e44a7f2588f06ffd7ab16744b4521b015
JuanDMeGon <juandmegon@gmail.com>
2017-02-18 11:15:17 -0500

    Implementando el Método Show en SellerController

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -28,6 +28,8 @@ class SellerController extends Controller
      */
     public function show($id)
     {
-        //
+        $vendedor = Seller::has('products')->findOrFail($id);
+
+        return response()->json(['data' => $vendedor], 200);
     }
 }
