705065dbb4c4e7178ee7bb4a0b460e67a824fb24
JuanDMeGon <juandmegon@gmail.com>
2017-02-23 13:53:41 -0500

    Controlando Excepciones de Método No Permitido

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -10,6 +10,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -69,6 +70,10 @@ class Handler extends ExceptionHandler
 
         if ($exception instanceof NotFoundHttpException) {
             return $this->errorResponse('No se encontró la URL especificada', 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('El método especificado en la petición no es válido', 405);
         }
 
         return parent::render($request, $exception);
