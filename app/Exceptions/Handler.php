40d8c88ef29380996c9719774670069abe076f8f
JuanDMeGon <juandmegon@gmail.com>
2017-02-23 14:34:38 -0500

    Manejando Excepciones Inesperadas

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -90,7 +90,12 @@ class Handler extends ExceptionHandler
             }
         }
 
-        return parent::render($request, $exception);
+        if (config('app.debug')) {
+            return parent::render($request, $exception);            
+        }
+
+        return $this->errorResponse('Falla inesperada. Intente luego', 500);
+
     }
 
     /**
