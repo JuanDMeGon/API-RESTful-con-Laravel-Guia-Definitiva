02a0f4ab262d9a8736246af833ad745a5e8694da
JuanDMeGon <juandmegon@gmail.com>
2017-02-24 21:55:53 -0500

    Implementando el Método Store de CategoryController

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -28,7 +28,16 @@ class CategoryController extends ApiController
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+        ];
+
+        $this->validate($request, $rules);
+
+        $category = Category::create($request->all());
+
+        return $this->showOne($category, 201);
     }
 
     /**
