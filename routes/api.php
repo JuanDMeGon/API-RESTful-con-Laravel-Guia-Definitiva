5547edcbfd0c77dba05d02464e1dc722558cd8a8
JuanDMeGon <juandmegon@gmail.com>
2017-03-01 00:28:26 -0500

    Implementando el Método Index para SellerProductController

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,7 @@ Route::resource('transactions.sellers', 'Transaction\TransactionSellerController
  */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
 
