bda9a577a628d7c2b647b1cff0388d0534c9c51c
JuanDMeGon <juandmegon@gmail.com>
2017-03-05 16:16:44 -0500

    Manejando Acciones Propensas a Errores

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -140,7 +140,9 @@ class UserController extends ApiController
             return $this->errorResponse('Este usuario ya ha sido verificado.', 409);
         }
 
-        Mail::to($user)->send(new UserCreated($user));
+        retry(5, function() use ($user) {
+            Mail::to($user)->send(new UserCreated($user));
+        }, 100);
 
         return $this->showMessage('El correo de verificación se ha reenviado');
 
