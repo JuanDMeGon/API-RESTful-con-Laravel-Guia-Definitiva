fd7e2d29307962b02fcf7e640e27cfa2ca2c733e
JuanDMeGon <juandmegon@gmail.com>
2017-03-05 00:39:14 -0500

    Enviar Correo de Verificiación Usando Eventos

--- app/Mail/UserCreated.php
+++ app/Mail/UserCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
@@ -30,6 +31,6 @@ class UserCreated extends Mailable
      */
     public function build()
     {
-        return $this->text('emails.welcome');
+        return $this->text('emails.welcome')->subject('Por favor confirma tu correo electrónico');
     }
 }
