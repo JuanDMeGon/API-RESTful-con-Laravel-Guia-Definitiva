fafcb105f74db3243be1c19ec41b89e6ac3fe396
JuanDMeGon <juandmegon@gmail.com>
2017-03-07 14:24:48 -0500

    Creando la Transformación para Category

---
+++ app/Transformers/CategoryTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Category;
+use League\Fractal\TransformerAbstract;
+
+class CategoryTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Category $category)
+    {
+        return [
+            'identificador' => (int)$category->id,
+            'titulo' => (string)$category->name,
+            'detalles' => (string)$category->description,
+            'fechaCreacion' => (string)$category->created_at,
+            'fechaActualizacion' => (string)$category->updated_at,
+            'fechaEliminacion' => isset($category->updated_at) ? (string) $category->deleted_at : null,
+        ];
+    }
+}
