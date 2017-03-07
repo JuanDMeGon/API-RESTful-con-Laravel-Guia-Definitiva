62d44153377e06829b42846a461979df5dfdaf5a
JuanDMeGon <juandmegon@gmail.com>
2017-03-07 14:32:31 -0500

    Creando la Transformación para Transaction

---
+++ app/Transformers/TransactionTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Transaction;
+use League\Fractal\TransformerAbstract;
+
+class TransactionTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Transaction $transaction)
+    {
+        return [
+            'identificador' => (int)$transaction->id,
+            'cantidad' => (int)$transaction->quantity,
+            'comprador' => (int)$transaction->buyer_id,
+            'producto' => (int)$transaction->product_id,
+            'fechaCreacion' => (string)$transaction->created_at,
+            'fechaActualizacion' => (string)$transaction->updated_at,
+            'fechaEliminacion' => isset($transaction->updated_at) ? (string) $transaction->deleted_at : null,
+        ];
+    }
+}
