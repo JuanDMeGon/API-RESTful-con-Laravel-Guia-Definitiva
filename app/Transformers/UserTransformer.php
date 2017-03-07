a73e73e4f72226d4fded4748f45897e000258348
JuanDMeGon <juandmegon@gmail.com>
2017-03-06 22:58:21 -0500

    Creando la Transformación para User

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'identificador' => (int)$user->id,
+            'nombre' => (string)$user->name,
+            'correo' => (string)$user->email,
+            'esVerificado' => (int)$user->verified,
+            'esAdministrador' => ($user->admin === 'true'),
+            'fechaCreacion' => (string)$user->created_at,
+            'fechaActualizacion' => (string)$user->updated_at,
+            'fechaEliminacion' => isset($user->updated_at) ? (string) $user->deleted_at : null,
+        ];
+    }
+}
