224c4cfb49aa413de1dcba2562b897155e5432f6
JuanDMeGon <juandmegon@gmail.com>
2017-03-10 18:57:51 -0500

    Implementando HATEOAS para Instancias de Category

--- app/Transformers/CategoryTransformer.php
+++ app/Transformers/CategoryTransformer.php
@@ -21,6 +21,29 @@ class CategoryTransformer extends TransformerAbstract
             'fechaCreacion' => (string)$category->created_at,
             'fechaActualizacion' => (string)$category->updated_at,
             'fechaEliminacion' => isset($category->deleted_at) ? (string) $category->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('categories.show', $category->id),
+                ],
+                [
+                    'rel' => 'category.buyers',
+                    'href' => route('categories.buyers.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.products',
+                    'href' => route('categories.products.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.sellers',
+                    'href' => route('categories.sellers.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.transactions',
+                    'href' => route('categories.transactions.index', $category->id),
+                ],
+            ],
         ];
     }
 
