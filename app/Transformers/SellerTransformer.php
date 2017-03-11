2cb97dff9f2b284cb2de0fab5a5e20df5d831f1f
JuanDMeGon <juandmegon@gmail.com>
2017-03-10 19:33:23 -0500

    [Ejercicio] Implementando HATEOAS para Instancias de Seller

--- app/Transformers/SellerTransformer.php
+++ app/Transformers/SellerTransformer.php
@@ -22,6 +22,33 @@ class SellerTransformer extends TransformerAbstract
             'fechaCreacion' => (string)$seller->created_at,
             'fechaActualizacion' => (string)$seller->updated_at,
             'fechaEliminacion' => isset($seller->deleted_at) ? (string) $seller->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('sellers.show', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.buyers',
+                    'href' => route('sellers.buyers.index', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.categories',
+                    'href' => route('sellers.categories.index', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.products',
+                    'href' => route('sellers.products.index', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.transactions',
+                    'href' => route('sellers.transactions.index', $seller->id),
+                ],
+                [
+                    'rel' => 'user',
+                    'href' => route('users.show', $seller->id),
+                ],
+            ],
         ];
     }
 
