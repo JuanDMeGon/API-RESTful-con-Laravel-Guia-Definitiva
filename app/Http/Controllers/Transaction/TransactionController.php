ee88838d4b6f830f9432b88008684d190d1ef8f4
JuanDMeGon <juandmegon@gmail.com>
2017-07-19 17:00:14 -0500

    Implementando las Restricciones Sobre Transaction

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -12,6 +12,7 @@ class TransactionController extends ApiController
     {
         parent::__construct();
         $this->middleware('scope:read-general')->only('show');
+        $this->middleware('can:view,transaction')->only('show');
     }
     
     /**
