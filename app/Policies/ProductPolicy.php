fc513c8f81f3464861b8ec900106dcd7e03aafa0
JuanDMeGon <juandmegon@gmail.com>
2017-07-20 20:57:30 -0500

    Permitiendo las Acciones a los Administradores

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -4,11 +4,12 @@ namespace App\Policies;
 
 use App\User;
 use App\Product;
+use App\Traits\AdminActions;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ProductPolicy
 {
-    use HandlesAuthorization;
+    use HandlesAuthorization, AdminActions;
 
     /**
      * Determine whether the user can add a category form a product.
